a8fdb39c855706980d919618bfe91957135fc9c7
leo.lu <lqz.leo@gmail.com>
2022-11-15 22:34:00 +0800

    add fiels

--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,11 @@
 import { App } from 'vue'
 import containerWidget, { schema as containerWidgets } from './widget/container-widget'
 
-import render, { getSchemas } from 'coder-vform-render'
+import render, { WidgetManager } from 'coder-vform-render'
 
 import 'coder-vform-render/dist/style.css'
 import vFormDesigner from './designer/index.vue'
-import { addBasicFieldSchema, addContainerWidgetSchema, addAdvancedFieldSchema, addCustomWidgetSchema } from './widgetsConfig'
+
 import Draggable from 'vuedraggable-es'
 import { registerIcon } from './utils/el-icons'
 import { addDirective } from './utils/directive'
@@ -13,12 +13,7 @@ import { addDirective } from './utils/directive'
 import 'virtual:svg-icons-register'
 import '@/iconfont/iconfont.css'
 
-export const SchemaInstaller = {
-    addBasicFieldSchema,
-    addContainerWidgetSchema,
-    addAdvancedFieldSchema,
-    addCustomWidgetSchema
-}
+
 
 const install = (app: any) => {
 
@@ -29,17 +24,10 @@ const install = (app: any) => {
 
     //schema 处理
     containerWidgets.forEach((value: object) => {
-        addContainerWidgetSchema(value);
+        WidgetManager.addContainerWidgetSchema(value);
     })
 
-    getSchemas().forEach((value: object) => {
-        addBasicFieldSchema(value);
-    })
-
-  
     app.component('draggable', Draggable)
-
-
 
 }
 vFormDesigner.install = install;
