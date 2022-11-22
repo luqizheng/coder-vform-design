13eb255b14dd1f24303b8b403bffe37cc71f71cf
leo.lu <lqz.leo@gmail.com>
2022-11-22 15:05:54 +0800

    up to 6.0 and include render

--- lib/index.ts
+++ lib/index.ts
@@ -3,16 +3,14 @@ import containerWidget from './widget/container-widget'
 
 import render from 'coder-vform-render'
 import 'coder-vform-render/dist/style.css'
-import vFormDesigner from './designer/index.vue'
 
+import vFormDesigner from './designer/index.vue'
 import Draggable from 'vuedraggable-es'
 import { registerIcon } from './utils/el-icons'
 import { addDirective } from './utils/directive'
 
 import 'virtual:svg-icons-register'
 import '@/iconfont/iconfont.css'
-
-
 
 const install = (app: any) => {
     app.use(containerWidget);
